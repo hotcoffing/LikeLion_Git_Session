7ee2dee40c6223ef21a6a76371a6785cb4671a6f
KimSeongBin <skuy369@gmail.com>
2026-05-07 17:50:46 +0900

    week6 commit

---
+++ 4주차/week4_practice/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      {/* type="button"을 명시하여 시맨틱한 마크업을 유지합니다. */}
+      <button type="button" className="button">
+        Click me!
+      </button>
+    </div>
+  );
+}
+
+export default App;
